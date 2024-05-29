636fac12dc2deb2df035505c52542ddd8881e304
mahwiz khalil <77918472+mwzkhalil@users.noreply.github.com>
2024-05-29 01:53:15 -0700

    Update object_detector.py

--- object_detector.py
+++ object_detector.py
@@ -1,9 +1,8 @@
 from ultralytics import YOLO
-from flask import request, Flask, jsonify
+from flask import request, Flask, send_file
 from waitress import serve
 from PIL import Image, ImageDraw
 from io import BytesIO
-import base64
 
 from pyngrok import ngrok
 from flask_cors import CORS
@@ -19,34 +18,32 @@ def detect():
     """
     Handler of /detect POST endpoint.
     Receives uploaded file with a name "image_file", passes it
-    through YOLOv8 object detection network, and returns an array
-    of bounding boxes.
-    :return: a JSON array of objects bounding boxes in format [[x1,y1,x2,y2,object_type,probability],..]
+    through YOLOv8 object detection network, and returns the image
+    with bounding boxes drawn on it.
+    :return: The image with bounding boxes as a file response.
     """
     buf = request.files["image_file"]
-    boxes, img_with_boxes = detect_objects_on_image(buf.stream)
+    img_with_boxes = detect_objects_on_image(buf.stream)
     
-    # Convert image to base64
-    buffered = BytesIO()
-    img_with_boxes.save(buffered, format="JPEG")
-    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    # Save the image to a BytesIO object
+    img_io = BytesIO()
+    img_with_boxes.save(img_io, format="JPEG")
+    img_io.seek(0)
     
-    return jsonify({"boxes": boxes, "image": img_str})
+    return send_file(img_io, mimetype='image/jpeg')
 
 def detect_objects_on_image(buf):
     """
     Function receives an image,
     passes it through YOLOv8 neural network,
-    and returns an array of detected objects
-    and their bounding boxes.
+    and draws bounding boxes on the image.
     :param buf: Input image file stream
-    :return: Array of bounding boxes in format [[x1,y1,x2,y2,object_type,probability],..]
+    :return: Image with bounding boxes drawn on it.
     """
     model = YOLO("best.pt")
     image = Image.open(buf)
     results = model.predict(image)
     result = results[0]
-    output = []
 
     # Draw bounding boxes on image
     draw = ImageDraw.Draw(image)
@@ -55,11 +52,10 @@ def detect_objects_on_image(buf):
         class_id = box.cls[0].item()
         prob = round(box.conf[0].item(), 2)
         prob_percentage = f"{prob * 100:.2f}%"
-        output.append([x1, y1, x2, y2, result.names[class_id], prob_percentage])
         draw.rectangle([x1, y1, x2, y2], outline="red", width=3)
         draw.text((x1, y1), f"{result.names[class_id]} {prob_percentage}", fill="red")
     
-    return output, image
+    return image
 
 print(public_url)
 app.run()
